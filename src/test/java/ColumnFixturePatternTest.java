eebcb39150d0e9169504557538100ab3607e7d19
yimkong <17665448577@163.com>
2020-12-04 23:42:01 +0800

    update column fixture test

--- src/test/java/ColumnFixturePatternTest.java
+++ src/test/java/ColumnFixturePatternTest.java
@@ -8,17 +8,26 @@ import java.util.regex.Pattern;
 public class ColumnFixturePatternTest {
 
     private static Pattern regexMethodPattern = Pattern.compile("(.+)(?:\\?\\?|!!)");
+    //greedy match, only the last one match () or ? or ! is ok
     private static Pattern methodPattern = Pattern.compile("(.+)(?:\\(\\)|\\?|!)");
     private static Pattern fieldPattern = Pattern.compile("=?([^=]+)=?");
+    //
+    private static final Pattern disgracefulNamePattern = Pattern
+            .compile("\\w(?:[.]|\\w)*[^.]");
 
     public static void main(String[] args) {
         System.err.println(Pattern.matches("\\d+\\.\\d", "5.0"));
         System.err.println(regexMethodPattern.matcher("any??").matches());
         System.err.println(regexMethodPattern.matcher("any!!").matches());
         System.err.println(methodPattern.matcher("any()?").matches());
+        System.err.println(methodPattern.matcher("any()!!").matches());
         System.err.println(methodPattern.matcher("any()!").matches());
+        System.err.println(methodPattern.matcher("any?").matches());
         System.err.println(fieldPattern.matcher("=any=").matches());
         System.err.println(fieldPattern.matcher("=any").matches());
         System.err.println(fieldPattern.matcher("any=").matches());
+        System.err.println(disgracefulNamePattern.matcher("abc...ab").matches());
+        System.err.println(disgracefulNamePattern.matcher("abcab").matches());
+        System.err.println(disgracefulNamePattern.matcher("=abcab=").matches());
     }
 }
