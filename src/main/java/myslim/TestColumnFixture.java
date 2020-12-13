7a365c168a50cc8c299d8c241a9ce500c1c5e0fb
yimkong <17665448577@163.com>
2020-12-13 20:32:55 +0800

    little optimization

---
+++ src/main/java/myslim/TestColumnFixture.java
@@ -0,0 +1,32 @@
+package myslim;
+
+/**
+ * author yg
+ * description
+ * simple test
+ * date 2020/11/4
+ */
+public class TestColumnFixture {
+    private double numerator;
+    private double denominator;
+
+    public double quotient() {
+        return numerator / denominator;
+    }
+
+    public double getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(double numerator) {
+        this.numerator = numerator;
+    }
+
+    public double getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(double denominator) {
+        this.denominator = denominator;
+    }
+}
--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -Dproject_home="C:\project\fitnesse-learn" -cp 'lib\fitlibrary-2.0.jar;lib\fitnesse-20200501-standalone.jar;lib\fitnesse-learn.jar' fitnesseMain.FitNesseMain -v
+java -Dproject_home="C:\project\fitnesse-learn" -cp 'lib\fitlibrary-2.0.jar;lib\fitnesse-20200501-standalone.jar;lib\fitnesse-learn.jar' fitnesseMain.FitNesseMain -v %*
